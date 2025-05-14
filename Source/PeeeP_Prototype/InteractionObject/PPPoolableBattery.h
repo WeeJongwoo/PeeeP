982ee07bce257979b6151b70dbbac5196ca7f3cd
PirA <wjw0518@gmail.com>
2025-05-15 01:15:05 +0900

    풀링용 베터리 헤더 파일 복구

---
+++ Source/PeeeP_Prototype/InteractionObject/PPPoolableBattery.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InteractionObject/PPBattery.h"
+#include "Interface/PPPoolableInterface.h"
+#include "PPPoolableBattery.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PEEEP_PROTOTYPE_API APPPoolableBattery : public APPBattery, public IPPPoolableInterface
+{
+	GENERATED_BODY()
+	
+public:
+	APPPoolableBattery();
+
+	void SetObjectPool(class UPPPoolObject* InObjedtPool) override;
+	void ReturnSelf() override;
+	void SetActive(bool IsActive) override;
+
+private:
+
+	class UPPPoolObject* ObjectPool;
+};
