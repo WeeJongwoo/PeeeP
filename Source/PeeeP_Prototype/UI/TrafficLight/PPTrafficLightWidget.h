fa2704e9972b2c4b32e2d731f0d2c10798af1401
RaccPark <p48754250@gmail.com>
2025-06-19 23:38:49 +0900

    0619 신호등 UI 리소스 추가

---
+++ Source/PeeeP_Prototype/UI/TrafficLight/PPTrafficLightWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Inventory/PPCustomWidget.h"
+#include "PPTrafficLightWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PEEEP_PROTOTYPE_API UPPTrafficLightWidget : public UPPCustomWidget
+{
+	GENERATED_BODY()
+	
+};
