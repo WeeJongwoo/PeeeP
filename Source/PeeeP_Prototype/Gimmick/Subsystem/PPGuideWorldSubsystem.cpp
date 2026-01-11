2e19757f03660e27478079ea5b346aa7d194f319
Wee <wjw0518@gmail.com>
2026-01-12 00:30:06 +0900

    가이드 기능 수정
    3초간 가이드 보여주고 사라짐
    캐릭터 BeginePlay 부분에 가이드 Active, Hidden 델리게이트 연결부분 있음 참고해서 기능 연결하면 됨

--- Source/PeeeP_Prototype/Gimmick/Subsystem/PPGuideWorldSubsystem.cpp
+++ Source/PeeeP_Prototype/Gimmick/Subsystem/PPGuideWorldSubsystem.cpp
@@ -3,6 +3,7 @@
 
 #include "Gimmick/Subsystem/PPGuideWorldSubsystem.h"
 #include "Component/PPGuideObjectComponent.h"
+#include "TimerManager.h"
 
 void UPPGuideWorldSubsystem::RegisterGuideObject(UPPGuideObjectComponent* GuideObject)
 {
@@ -14,13 +15,42 @@ void UPPGuideWorldSubsystem::UnregisterGuideObject(UPPGuideObjectComponent* Guid
 	GuideObjectSet.Remove(GuideObject);
 }
 
-void UPPGuideWorldSubsystem::SetGuideActive(bool Active)
+void UPPGuideWorldSubsystem::GuideActive()
 {
+	if (GetWorld()->GetTimerManager().IsTimerActive(GuideTimerHandle))
+	{
+		return;
+	}
+
 	for (TWeakObjectPtr<UPPGuideObjectComponent> WeakGuideObjectPtr : GuideObjectSet)
 	{
 		if (UPPGuideObjectComponent* GuideObjectPtr = WeakGuideObjectPtr.Get())
 		{
-			GuideObjectPtr->GuideSetHidden(!Active);
+			GuideObjectPtr->GuideSetHidden(false);
+
 		}
 	}
+	OnGuideActiveDelegate.Broadcast();
+
+	TWeakObjectPtr<UPPGuideWorldSubsystem> ThisWeakPtr(this);
+
+	FTimerDelegate GuideTimerDelegate;
+	GuideTimerDelegate.BindLambda([ThisWeakPtr]() {
+
+		if (UPPGuideWorldSubsystem* ThisPtr = ThisWeakPtr.Get())
+		{
+			for (TWeakObjectPtr<UPPGuideObjectComponent> WeakGuideObjectPtr : ThisPtr->GuideObjectSet)
+			{
+				if (UPPGuideObjectComponent* GuideObjectPtr = WeakGuideObjectPtr.Get())
+				{
+					GuideObjectPtr->GuideSetHidden(true);
+				}
+			}
+
+			ThisPtr->OnGuideHiddenDelegate.Broadcast();
+			ThisPtr->GetWorld()->GetTimerManager().ClearTimer(ThisPtr->GuideTimerHandle);
+		}
+	});
+
+	GetWorld()->GetTimerManager().SetTimer(GuideTimerHandle, GuideTimerDelegate, 3.0f, false);
 }
